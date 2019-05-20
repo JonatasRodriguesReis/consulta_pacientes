dd41245bfce49199d3293a2655d066dbc89687c4
JonatasRodriguesReis <jonatas.reis@icomp.ufam.edu.br>
2019-05-20 13:20:14 -0400

    Alteracoes

--- src/store.js
+++ src/store.js
@@ -1,50 +1,146 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import * as firebase from 'firebase'
-//import { stat } from 'fs';
 import VuexPersistence from 'vuex-persist'
-
 
 Vue.use(Vuex)
 
 const vuexLocal = new VuexPersistence({
-    storage: window.localStorage
+  storage: window.localStorage
 })
 
 export const store = new Vuex.Store({
   plugins: [vuexLocal.plugin],
   state: {
-    medicos: []
+    loadedProntuarios: [],
+    medicos: [],
+    pacientes: [],
+    dados: [],
   },
+
   mutations: {
     clear_medicos(state){
       state.medicos = []
     },
 
     add_medico(state, payload){
-      state.medicos.push( {... payload})      
+      state.medicos.push( {... payload})
+    },
+
+    setDados(state, payload) {
+      console.log('Passo 3')
+      console.log(payload)
+      state.dados = payload
     }
   },
-  actions: {
-      getMedicos({commit}){
-        firebase.database().ref('/medicos').once('value').then(function(snapshot) {
-          commit('clear_medicos')
-          var obj = snapshot.val();
-          for (const key in obj) {
-            commit('add_medico',{
-              id: key,
-              ...
-              obj[key],
 
-            })
-          }
-        });
+  actions: {
+    criarProntuario ({commit}, payload) {
+      const prontuario = {
+        exames: payload.exames,
+        hipotesesDiagnosticas: payload.hipotesesDiagnosticas,
+        diagnosticosDefinidos: payload.diagnosticosDefinidos,
+        tratamentosEfetuados: payload.tratamentosEfetuados,
+        data: payload.data,
       }
+      firebase.database().ref('prontuario').push(prontuario)
+          .then((data) => {
+            console.log(data);
+            commit ('criarProntuario', prontuario)
+          })
+          .catch((error) => {
+            console.log(error)
+          })
+    },
+
+    createMedicos({commit}, payload) {
+      var ref = firebase.database().ref('/medicos');
+      ref.child(payload.crm).set(payload)
+    },
+
+    loadDados ({commit}) {
+      firebase.database().ref('medicos').once('value')
+          .then((data) => {
+            const medico = []
+            console.log('Passo 1')
+            console.log(data.val())
+            const obj = data.val()
+            for (let key in obj) {
+              medico.push({
+                nome: obj[key].nome,
+                crm: obj[key].crm,
+                especialidades: obj[key].especialidades
+              })
+            }
+            console.log('Passo 2')
+            console.log(medico)
+            commit('setDados', medico)
+          })
+          .catch(
+              (error) => {
+                console.log(error)
+                commit('setDados', false)
+              }
+          )
+    },
+
+    createPacientes({commit}, payload) {
+      var ref = firebase.database().ref('/pacientes');
+      ref.child(payload.cpf).set(payload)
+    },
+
+    addDatesTimeVacancy({commit}, payload){
+      //var keyMed = {medico: payload.medicos}
+      //var keyEspe = {especialidades: payload.especialidades}
+      var infos={
+        datas: payload.datas,
+        horas: payload.horas,
+        vagas: payload.vagas
+      }
+
+      console.log(infos)
+      firebase.database().ref('/medicos/'+ payload.idMed + '/especialidades/' + payload.espMed +'/').child(payload.idMed).set(infos)
+
+      for(const key in payload.datas){
+        firebase.database().ref('/medicos/'+ payload.idMed + '/especialidades/' + payload.espMed +'/consultas').child(payload.idMed).set(
+            {data_inicial:payload.datas[key] +'T'+ payload.horas})
+
+      }
+    },
+
+    getMedicos({commit}){
+      firebase.database().ref('/medicos').once('value').then(function(snapshot) {
+        commit('clear_medicos')
+        var obj = snapshot.val();
+        for (const key in obj) {
+          commit('add_medico',{
+            id: key,
+            ...
+                obj[key],
+          })
+        }
+      });
+    },
+
+    agendarConsulta({commit},payload){
+      const paciente = {
+        nome: payload.nome,
+        num_associado: payload.num_associado,
+        status: payload.status,
+        modalidade: payload.modalidade
+      };
+      firebase.database().ref(payload.path).push(paciente);
+    }
   },
+
   getters:{
+    dados (state) {
+      console.log('Passo 5')
+      console.log(state.dados)
+      return state.dados
+    },
     get_especialidades(state){
       var especialidades = []
-      //especialidades.ind
       state.medicos.forEach(element => {
         for (const key in element.especialidades) {
           especialidades.push({text: key})
@@ -66,41 +162,79 @@ export const store = new Vuex.Store({
           nome_medicos.push(element.nome)
         }
       });
-
       return nome_medicos;
     },
-    get_Datas: (state) => (args) => {
-       var datas = []
-
-       console.log(args.medico)
-
-       state.medicos.forEach(element => {
-
-        if( element.nome == args.medico ){
-          for (const key in element.especialidades) {
-            if(key == args.especialidade){
-              console.log('hjg')
-              for(const key2 in element.especialidades[key].consultas){
-                console.log(element.especialidades[key].consultas[key2])
-              }
-
-            }
-          }
-        }
-
-        /*var tem = false
+    get_CRM: (state) => (args) => {
+      var crm = ''
+      state.medicos.forEach(element => {
+        var tem = false
         for (const key in element.especialidades) {
-          if(key == especialidade){
+          if(key == args.especialidade){
             tem = true
             break;
           }
         }
-        if(tem){
-          nome_medicos.push(element.nome)
-        }*/
-      });
+        if(tem && element.nome == args.medico){
+          crm = element.id
 
-      return datas;
+        }
+      });
+      return crm;
+    },
+
+    get_Datas: (state) => (args) => {
+      var datas = []
+      console.log(args.medico)
+      state.medicos.forEach( element => {
+        if(element.nome == args.medico){
+          for(const key in element.especialidades){
+            if(key == args.especialidade){
+              for(const key2 in element.especialidades[key].consultas){
+                datas.push(element.especialidades[key].consultas[key2].data_inicial.split('T')[0])
+              }
+            }
+          }
+        }
+      })
+      return datas
+    },
+    get_Horas: (state) => (args) => {
+      var timesOptions = []
+      console.log(args.medico)
+      state.medicos.forEach( element => {
+        if(element.nome == args.medico){
+          for(const key in element.especialidades){
+            if(key == args.especialidade){
+              for(const key2 in element.especialidades[key].consultas){
+                //Condição que verifica também se já tem paciente a consulta. Caso tive, não aparecerá no calendário porque a consulta já está agendada
+                if(element.especialidades[key].consultas[key2].data_inicial.split('T')[0] == args.data
+                    && element.especialidades[key].consultas[key2].paciente == undefined){
+                  timesOptions.push(element.especialidades[key].consultas[key2].data_inicial.split('T')[1])
+                }
+              }
+            }
+          }
+        }
+      })
+      return timesOptions
+    },
+    get_Path_Cadastro_Cons: (state) => (args) => {
+      var path = ''
+      state.medicos.forEach( element => {
+        if(element.nome == args.medico){
+          for(const key in element.especialidades){
+            if(key == args.especialidade){
+              for(const key2 in element.especialidades[key].consultas){
+                if(element.especialidades[key].consultas[key2].data_inicial.split('T')[0] == args.data
+                    && element.especialidades[key].consultas[key2].data_inicial.split('T')[1] == args.hora){
+                  path = '/medicos/' + element.id + '/especialidades/' + key + '/consultas/' + key2 + '/paciente'
+                }
+              }
+            }
+          }
+        }
+      })
+      return path
     }
   }
 })
--- src/views/calendario_medicos.vue
+++ src/views/calendario_medicos.vue
@@ -41,7 +41,6 @@
                         <v-date-picker
                                 v-model="dates"
                                 multiple
-                                :locale="pt-br"
                         ></v-date-picker>
                     </v-flex>
                     <v-flex xs12 sm6>
@@ -71,7 +70,7 @@
                                         v-on="on"
                                 ></v-combobox>
                             </template>
-                            <v-date-picker v-model="dates" multiple :locale="pt-br" no-title scrollable>
+                            <v-date-picker v-model="dates" multiple no-title scrollable>
                                 <v-spacer></v-spacer>
                                 <v-btn flat color="primary" @click="menu = false">Cancelar</v-btn>
                                 <v-btn flat color="primary" @click="$refs.menu.save(dates)">Salvar</v-btn>
@@ -83,27 +82,40 @@
             </template>
 
             <template>
-                <v-container>
-                    <v-flex>
-                        <v-select
-                                v-model="times"
-                                prepend-icon="alarm_add"
-                                :items="timesOptions"
-                                label="Horários"
-                                multiple
-                                attach
-                                outline
-                                chips
-                                clearable
-                                color="success"
-                        ></v-select>
-                    </v-flex>
+                <v-container fluid grid-list-xl>
+                    <v-layout align-center wrap>
+                        <v-flex xs12 sm6>
+                            <v-select
+                                    v-model="times"
+                                    prepend-icon="alarm_add"
+                                    :items="timesOptions"
+                                    label="Horários"
+                                    attach
+                                    outline
+                                    chips
+                                    clearable
+                            ></v-select>
+                        </v-flex>
+                        <v-spacer></v-spacer>
+                        <v-flex xs6 sm6 md6>
+                            <v-text-field
+                                    prepend-icon="looks_6"
+                                    v-model="vagas"
+                                    :mask="maskVAGAS"
+                                    label="Número de Vagas"
+                                    placeholder="exemplo: 22"
+                                    outline
+                                    clearable
+                            ></v-text-field>
+                        </v-flex>
+                    </v-layout>
                     {{times}}
+                    {{vagas}}
                 </v-container>
             </template>
 
             <v-layout align-end justify-end>
-                <v-btn v-if="!alert" @click="alert = true" outline flat color="blue">
+                <v-btn @click="saveDatesTimeVacancy" outline flat color="blue">
                     <v-icon left="">save</v-icon>
                     SALVAR
                 </v-btn>
@@ -129,86 +141,84 @@
 <script>
     export default {
         data: () => ({
-            dates: [],
-            alert: false,
             menu: false,
+            alert: false,
+            maskVAGAS:'##',
+            vagas:'',
+            dates: [],
+            medicos: '',
+            medicosOptions: [],
+            especialidade: '',
             times: '',
             timesOptions: [
                 { text: '06:00'},
-                { text: '06:15'},
                 { text: '06:30'},
-                { text: '06:45'},
                 { text: '07:00'},
-                { text: '07:15'},
                 { text: '07:30'},
-                { text: '07:45'},
                 { text: '08:00'},
-                { text: '08:15'},
                 { text: '08:30'},
-                { text: '08:45'},
                 { text: '09:00'},
-                { text: '09:15'},
                 { text: '09:30'},
-                { text: '09:45'},
                 { text: '10:00'},
-                { text: '10:15'},
                 { text: '10:30'},
-                { text: '10:45'},
                 { text: '11:00'},
-                { text: '11:15'},
                 { text: '11:30'},
-                { text: '11:45'},
                 { text: '12:00'},
-                { text: '12:15'},
                 { text: '12:30'},
-                { text: '12:45'},
                 { text: '13:00'},
-                { text: '13:15'},
                 { text: '13:30'},
-                { text: '13:45'},
                 { text: '14:00'},
-                { text: '14:15'},
                 { text: '14:30'},
-                { text: '14:45'},
                 { text: '15:00'},
-                { text: '15:15'},
                 { text: '15:30'},
-                { text: '15:45'},
                 { text: '16:00'},
-                { text: '16:15'},
                 { text: '16:30'},
-                { text: '16:45'},
                 { text: '17:00'}
-            ],
-            medicos: '',
-            medicosOptions: [
-                { text: 'Ana Tereza de A. Vasques'},
-                { text: 'Brisa Silva Bracchi'},
-                { text: 'Debora Kaynara Patricio da Silva'},
-                { text: 'Glaucia Adriana Dantas Pereira'},
-                { text: 'Cleiton Alves Feitosa'},
-                { text: 'Diogo Fernandes de Souza'},
-                { text: 'Guilherme Azevedo Reis'},
-                { text: 'Ramon Douglas Neves de Andrade'},
-                { text: 'Lorena Alcântara de Farias'},
-                { text: 'Monique Gurgel Aguiar'},
-                { text: 'Anne Marie Alves Trindade'}
-            ],
-            especialidade: '',
-            especialidadeOptions: [
-                { text: 'Clínico Geral'},
-                { text: 'Nutricionista'},
-                { text: 'Psiquiatra'},
-                { text: 'Fonoaudiologia'},
-                { text: 'Odonotologia'},
-                { text: 'Neurologia'},
-                { text: 'Cardiologia'},
-                { text: 'Dermatologia'},
-                { text: 'Pediatria'},
-                { text: 'Urologia'},
-                { text: 'Oftalmologia'}
             ]
-        })
+        }),
+
+        computed:{
+            especialidadeOptions(){
+                return this.$store.getters.get_especialidades
+            }
+        },
+
+        watch: {
+            search (val) {
+                val && val !== this.select && this.querySelections(val)
+            },
+
+            especialidade: function(value){
+                this.medicosOptions = this.$store.getters.get_MedicosbyEspecialidade(value)
+            }
+        },
+
+        methods: {
+            saveDatesTimeVacancy () {
+                var crm = this.$store.getters.get_CRM({especialidade:this.especialidade,medico: this.medicos})
+                const medicoInfos = {
+                    datas: this.dates,
+                    horas: this.times,
+                    vagas: this.vagas,
+                    idMed: crm,
+                    espMed: this.especialidade
+                }
+                this.$store.dispatch('addDatesTimeVacancy', medicoInfos)
+
+                //this.$store.dispatch
+                this.$router.push('/')
+            },
+            querySelections (v) {
+                this.loading = true
+                // Simulated ajax query
+                setTimeout(() => {
+                    this.items = this.states.filter(e => {
+                        return (e || '').toLowerCase().indexOf((v || '').toLowerCase()) > -1
+                    })
+                    this.loading = false
+                }, 500)
+            },
+        }
     }
 </script>
 
